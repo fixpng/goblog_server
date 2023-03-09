3dbc13ee5b0a465e32892be2bae54748330643ec
FixPng <897491068@qq.com>
2023-03-09 19:31:05 +0800

    多配置接口修改

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -6,9 +6,32 @@ import (
 	"gvb_server/models/res"
 )
 
+type SettingsUri struct {
+	Name string `uri:"name"`
+}
+
+// SettingsInfoView 显示某一项的配置信息
 func (s SettingApi) SettingsInfoView(c *gin.Context) {
-	//res.Ok(map[string]string{}, "xxx", c)
-	//res.OkWithData(map[string]string{"id": "xxx"}, c)
-	//res.FailWithCode(res.SettingsError, c)
-	res.OkWithData(global.Config.SiteInfo, c)
+	var cr SettingsUri
+	err := c.ShouldBindUri(&cr)
+	if err != nil {
+		res.FailWithCode(res.ArgumentError, c)
+		return
+	}
+
+	switch cr.Name {
+	case "site":
+		res.OkWithData(global.Config.SiteInfo, c)
+	case "email":
+		res.OkWithData(global.Config.Email, c)
+	case "qq":
+		res.OkWithData(global.Config.QQ, c)
+	case "qiniu":
+		res.OkWithData(global.Config.QiNiu, c)
+	case "jwt":
+		res.OkWithData(global.Config.Jwy, c)
+	default:
+		res.FailWithMessage("没有对应的配置信息", c)
+	}
+
 }
--- api/settings_api/settings_update.go
+++ api/settings_api/settings_update.go
@@ -8,24 +8,62 @@ import (
 	"gvb_server/models/res"
 )
 
-func (s SettingApi) SettingsInfoUpdateView(c *gin.Context) {
-	var cr config.SiteInfo
-	err := c.ShouldBindJSON(&cr)
+// SettingsUpdateView 修改某一项的配置信息
+func (s SettingApi) SettingsUpdateView(c *gin.Context) {
+	var cr SettingsUri
+	err := c.ShouldBindUri(&cr)
 	if err != nil {
 		res.FailWithCode(res.ArgumentError, c)
 		return
 	}
-	global.Log.Infoln("修改前数据：", global.Config)
-	global.Config.SiteInfo = cr
-	global.Log.Infoln("修改后数据：", global.Config)
 
-	err = core.SetYaml()
-	if err != nil {
-		global.Log.Error(err)
-		res.FailWithMessage(err.Error(), c)
+	switch cr.Name {
+	case "site":
+		var info config.SiteInfo
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.SiteInfo = info
+	case "email":
+		var info config.Email
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.Email = info
+	case "qq":
+		var info config.QQ
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.QQ = info
+	case "qiniu":
+		var info config.QiNiu
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.QiNiu = info
+	case "jwt":
+		var info config.Jwy
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.Jwy = info
+	default:
+		res.FailWithMessage("没有对应的配置信息", c)
 		return
 	}
 
+	core.SetYaml()
 	res.OkWith(c)
 
 }
