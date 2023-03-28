db122c45c89fd995730c0a247e171208365ec3e0
FixPng <897491068@qq.com>
2023-03-28 09:46:06 +0800

    消息管理：个人消息列表(有bug未处理

--- service/common/service_list.go
+++ service/common/service_list.go
@@ -19,7 +19,7 @@ func ComList[T any](model T, option Option) (list []T, count int64, err error) {
 		DB = global.DB.Session(&gorm.Session{Logger: global.MysqlLog})
 	}
 	if option.Sort == "" {
-		option.Sort = "create_at desc" // 默认按照创建时间从大到小
+		option.Sort = "created_at desc" // 默认按照创建时间从大到小
 	}
 
 	query := DB.Where(model)
