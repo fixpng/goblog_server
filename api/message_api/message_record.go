7a1b4969f9e4792eef27050603ffe0c2973e3224
FixPng <897491068@qq.com>
2023-03-29 00:11:18 +0800

    消息管理：消息详情

---
+++ api/message_api/message_record.go
@@ -0,0 +1,43 @@
+package message_api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gvb_server/global"
+	"gvb_server/models"
+	"gvb_server/models/res"
+	"gvb_server/utils/jwts"
+)
+
+type MessageRecordRequest struct {
+	UserID uint `json:"user_id" binding:"required" msg:"请输入查询的用户id"`
+}
+
+func (MessageApi) MessageRecordView(c *gin.Context) {
+	var cr MessageRecordRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		res.FailWithError(err, &cr, c)
+		return
+	}
+	_claims, _ := c.Get("claims")
+	claims := _claims.(*jwts.CustomClaims)
+
+	var _messageList []models.MessageModel
+	var messageList = make([]models.MessageModel, 0)
+	global.DB.Order("created_at asc").
+		Find(&_messageList, "send_user_id = ? or rev_user_id = ?", claims.UserID, claims.UserID)
+	for _, model := range _messageList {
+		// 判断是一个组的条件
+		// send_user_id 和 rev_user_id 其中一个
+		// 1 2  2 1
+		// 1 3  3 1 是一组
+		if model.RevUserID == cr.UserID || model.SendUserID == cr.UserID {
+			messageList = append(messageList, model)
+		}
+	}
+
+	// 点开消息，里面的每一条消息，都从未读变成已读
+
+	res.OkWithData(messageList, c)
+	return
+}
--- routers/message_router.go
+++ routers/message_router.go
@@ -10,4 +10,5 @@ func (router RouterGroup) MessageRouter() {
 	router.POST("messages", middleware.JwtAuth(), app.MessageCreateView)
 	router.GET("messages_all", middleware.JwtAdmin(), app.MessageListAllView)
 	router.GET("messages", middleware.JwtAuth(), app.MessageListView)
+	router.GET("messages_record", middleware.JwtAuth(), app.MessageRecordView)
 }
