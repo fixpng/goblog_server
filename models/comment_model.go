f6e742f6f38d34d240aa1e081055e93dba019e03
FixPng <897491068@qq.com>
2023-04-28 00:33:41 +0800

    表结构迁移

--- models/comment_model.go
+++ models/comment_model.go
@@ -9,7 +9,7 @@ type CommentModel struct {
 	Content            string          `gorm:"size:256" json:"content"`                         // 评论内容
 	DiggCount          int             `gorm:"size:8;default:0;" json:"digg_count"`             // 点赞数
 	CommentCount       int             `gorm:"size:8;default:0;" json:"comment_count"`          // 子评论数
-	ArticleESID        string          `json:"article_es_id""`                                  // 文章id
+	ArticleID          string          `gorm:"size:32" json:"article_id""`                      // 文章id
 	User               UserModel       `json:"user"`                                            //关联的用户
 	UserID             uint            `json:"user_id"`                                         // 评论的用户
 }
