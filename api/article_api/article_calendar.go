715f33c3b55e8aac504a0ac7a6fa632ac9757318
FixPng <897491068@qq.com>
2023-04-30 01:05:50 +0800

    文章时间段搜索

--- api/article_api/article_calendar.go
+++ api/article_api/article_calendar.go
@@ -2,22 +2,46 @@ package article_api
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic/v7"
 	"gvb_server/global"
 	"gvb_server/models"
 	"gvb_server/models/res"
+	"time"
 )
+
+type CalendarDateResponse struct {
+	Date  string `json:"date"`
+	Count int    `json:"count"`
+}
+
+type BucketsType struct {
+	Buckets []struct {
+		KeyAsString string `json:"key_as_string"`
+		Key         int64  `json:"key"`
+		DocCount    int    `json:"doc_count"`
+	} `json:"buckets"`
+}
 
 func (ArticleApi) ArticleCalendarView(c *gin.Context) {
 
 	// 按时间聚合
 	agg := elastic.NewDateHistogramAggregation().Field("created_at").CalendarInterval("hour")
 
+	// 时间段搜索
+	// 从今天开始，到去年的今天
+	now := time.Now()
+	aYearAgo := now.AddDate(-1, 0, 0)
+	format := "2006-01-02 15:04:05"
+	// lt 小于 gt 大于
+	query := elastic.NewRangeQuery("created_at").
+		Gte(aYearAgo.Format(format)).
+		Lte(now.Format(format))
+
 	result, err := global.ESClient.
 		Search(models.ArticleModel{}.Index()).
-		Query(elastic.NewBoolQuery()).
+		Query(query).
 		Aggregation("calendar", agg).
 		Size(0).
 		Do(context.Background())
@@ -27,6 +51,23 @@ func (ArticleApi) ArticleCalendarView(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(string(result.Aggregations["calendar"]))
+	var data BucketsType
+	_ = json.Unmarshal(result.Aggregations["calendar"], &data)
+
+	var resList = make([]CalendarDateResponse, 0)
+
+	for _, bucket := range data.Buckets {
+		Time, _ := time.Parse(format, bucket.KeyAsString)
+		resList = append(resList, CalendarDateResponse{
+			Date:  Time.Format("2006-01-02"),
+			Count: bucket.DocCount,
+		})
+	}
+
+	//days := int(now.Sub(aYearAgo).Hours() / 24)
+	//for i := 0; i < days; i++ {
+	//	fmt.Println(aYearAgo.AddDate(0, 0, i))
+	//}
+	res.OkWithData(resList, c)
 
 }
