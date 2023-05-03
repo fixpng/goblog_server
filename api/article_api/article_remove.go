5e083f080a7d64d360d304f789885c8884f2191f
FixPng <897491068@qq.com>
2023-05-03 15:37:40 +0800

    全文搜索删除

--- api/article_api/article_remove.go
+++ api/article_api/article_remove.go
@@ -8,6 +8,7 @@ import (
 	"gvb_server/global"
 	"gvb_server/models"
 	"gvb_server/models/res"
+	"gvb_server/service/es_ser"
 )
 
 type IDListRequest struct {
@@ -23,10 +24,15 @@ func (ArticleApi) ArticleRemoveView(c *gin.Context) {
 		return
 	}
 
+	// 文章删除后，用户收藏过这篇文章如何处理?（代码待完善）
+	// 1.删除时把文章关联的收藏也删除
+	// 2.用户收藏表，新增一个字段，表示文章是否删除，用户可以删除这个收藏记录，但是找不到文章改收藏数
 	bulkService := global.ESClient.Bulk().Index(models.ArticleModel{}.Index()).Refresh("true")
 	for _, id := range cr.IDList {
 		req := elastic.NewBulkDeleteRequest().Id(id)
 		bulkService.Add(req)
+		// 异步全文搜索删除
+		go es_ser.DeleteFullTextByArticleID(id)
 	}
 	result, err := bulkService.Do(context.Background())
 	if err != nil {
