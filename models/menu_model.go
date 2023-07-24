284ab34b5174c168556f7022d2150c03a45afcc5
FixPng <897491068@qq.com>
2023-07-25 00:57:08 +0800

    菜单表path字段大小

--- models/menu_model.go
+++ models/menu_model.go
@@ -8,7 +8,7 @@ import (
 type MenuModel struct {
 	MODEL
 	Title        string        `gorm:"size:32" json:"title"`                                                                     // 标题
-	Path         string        `gorm:"size:32" json:"path"`                                                                      // 路径
+	Path         string        `gorm:"size:256" json:"path"`                                                                     // 路径
 	Slogan       string        `gorm:"size:64" json:"slogan"`                                                                    // slogan
 	Abstract     ctype.Array   `gorm:"type:string" json:"abstract"`                                                              // 简介
 	AbstractTime int           `json:"abstract_time"`                                                                            // 简介的切换时间
